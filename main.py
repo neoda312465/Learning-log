1206c075fd77ed48587a32d1ee3d610344de8ac3
neoda312465 <chiggugaming707@gmail.com>
2026-07-13 01:06:31 +0530

    Day-2: Pydantic models

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
-    return{"message": "Hello Neo!"}
+    return {"message": "Hello, Neo!"}
+
+class person(BaseModel):
+    name:str
+    age: int
+@app.post("/greet")
+def greet(person: person):
+    return{"message": f"Hello {person.name} bhai, you are {person.age} years old! Wow, you are young!"}
