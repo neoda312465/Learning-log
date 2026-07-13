47542fb30009dec7559212c09fc57dbab2ae869d
neoda312465 <chiggugaming707@gmail.com>
2026-07-14 01:45:47 +0530

    Dya-3: Path and Query params

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ class person(BaseModel):
 @app.post("/greet")
 def greet(person: person):
     return{"message": f"Hello {person.name} bhai, you are {person.age} years old! Wow, you are young!"}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int, verbose: bool = False):
+    if verbose:
+        return{"user_id": user_id, "detail": f"Full information about user {user_id}"}
+    return {"user_id": user_id}
